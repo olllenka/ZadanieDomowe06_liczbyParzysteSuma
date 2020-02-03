942eef4291f90fa75d050f519a2c9d2072ac5a08
Aleksandra Zieniewicz <olllenka.zie@gmail.com>
2020-02-03 23:31:09 +0100

    ready homework - even numbers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sum=0;
+        for(int i=0; i<=100; i+=2){
+            System.out.println(i);
+            sum+=i;
+        }
+        System.out.print("Suma liczb parzystych od 0 do 100 wynosi: " + sum);
+    }
+}
